3eb35194dddab815624298187e501f003e0b34f7
thanhnh99 <thanhnh99.amc@gmail.com>
2020-07-22 17:56:52 +0700

    add constructor and method for HTTP response

--- src/main/java/com/multiple_language_menu/models/responses/httpResponse/HttpResponse.java
+++ src/main/java/com/multiple_language_menu/models/responses/httpResponse/HttpResponse.java
@@ -1,9 +1,20 @@
 package com.multiple_language_menu.models.responses.httpResponse;
 
-import java.util.Collection;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public abstract class HttpResponse<T> {
+import java.util.ArrayList;
+import java.util.Collection;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HttpResponse<T>{
     private String statusCode;
     private String message;
-    private Collection<T> data;
+    private Collection<T> data = new ArrayList<T>();
+    public void addData( T data )
+    {
+        this.data.add(data);
+    }
 }
