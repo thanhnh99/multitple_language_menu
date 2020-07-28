c4daf12fa9d92e8f5e47ff617d0c60c847dbc0d9
thanhnh99 <thanhnh99.amc@gmail.com>
2020-07-28 11:04:55 +0700

    refactor request

--- src/main/java/com/multiple_language_menu/models/request/ReqCreateAdmin.java
+++ src/main/java/com/multiple_language_menu/models/request/ReqCreateAdmin.java
@@ -1,5 +1,12 @@
 package com.multiple_language_menu.models.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReqCreateAdmin {
     private String userName;
     private String password;
--- src/main/java/com/multiple_language_menu/models/request/ReqCreateShop.java
+++ src/main/java/com/multiple_language_menu/models/request/ReqCreateShop.java
@@ -1,21 +1,30 @@
 package com.multiple_language_menu.models.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReqCreateShop {
     private String ownerName;
     private String ownerPassword;
+    private String ownerEmail;
     private String shopName;
     private String address;
+    private String phone;
     private String openTime;
-    private String averagePrice;
     private String website;
     private Boolean wifi;
     private Integer parkingScale;
     private Boolean smokingPlace;
-    private Boolean booking;
+    private Boolean preOrder;
     private String description;
-//    private String coverImage;
-    private String contractTerm;
+    private String coverImage;
+    private Date contractTerm;
     private List<String> paymentMethod;
 }
