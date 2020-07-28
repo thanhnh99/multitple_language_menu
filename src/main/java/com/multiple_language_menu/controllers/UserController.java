d40b0117e06c6bb06f8ea824e6d5b8755a291e34
thanhnh99 <thanhnh99.amc@gmail.com>
2020-07-28 11:06:12 +0700

    add Admin controller

--- src/main/java/com/multiple_language_menu/controllers/UserController.java
+++ src/main/java/com/multiple_language_menu/controllers/UserController.java
@@ -1,4 +1,40 @@
 package com.multiple_language_menu.controllers;
 
+import com.multiple_language_menu.models.request.ReqCreateAdmin;
+import com.multiple_language_menu.models.responses.dataResponse.ResCreateManager;
+import com.multiple_language_menu.models.responses.httpResponse.HttpResponse;
+import com.multiple_language_menu.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("user")
+
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    @PostMapping
+    @PreAuthorize("@appAuthorizer.authorize(authentication, {'root'})")
+    public ResponseEntity addAdmin(HttpServletRequest request,
+                                   @RequestBody ReqCreateAdmin requestData)
+    {
+        HttpResponse response = new HttpResponse();
+        if(userService.createAdmin(request,requestData) != null)
+        {
+            response.setStatusCode("200");
+            response.setMessage("success");
+            return ResponseEntity.ok(response);
+        }
+        response.setStatusCode("400");
+        response.setMessage("bad request");
+        return ResponseEntity.ok(response);
+    }
 }
