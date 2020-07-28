6f2bb55c8496cce68bc9e15cc603cef9f193833c
thanhnh99 <thanhnh99.amc@gmail.com>
2020-07-28 17:31:02 +0700

    fix logic service getSHops

--- src/main/java/com/multiple_language_menu/services/ShopService.java
+++ src/main/java/com/multiple_language_menu/services/ShopService.java
@@ -11,9 +11,13 @@ import com.multiple_language_menu.repositories.IShopRepository;
 import com.multiple_language_menu.repositories.IUserRepository;
 import com.multiple_language_menu.services.authorize.AttributeTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import javax.servlet.http.HttpServletRequest;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -80,7 +84,7 @@ public class ShopService {
                             new ArrayList<>(),
                             new ArrayList<>());
                     shop.setLanguages(languageService.getLanguages());
-                    shop.setCreated_by(manager.getCreated_by());
+                    shop.setCreatedBy(manager.getCreatedBy());
                     shopRepository.save(shop);
                     languageService.addLanguage(shop.getId());
                     return true;
@@ -103,6 +107,7 @@ public class ShopService {
     }
 
 
+
     public List<ResShop> getShops(HttpServletRequest httpRequest)
     {
         //TODO: getShop logic
@@ -114,6 +119,7 @@ public class ShopService {
             String endDate = httpRequest.getParameter("endDate");
 
             List<ResShop> response = new ArrayList<ResShop>();
+            Page<Shops> shopsEntity = null;
             String token = httpRequest.getHeader("Authorization");
             String username = AttributeTokenService.getUsernameFromToken(token);
 
@@ -122,77 +128,113 @@ public class ShopService {
                 //Err = pagesize and page is required fields
                 return null;
             }
-//            else if(startDate == null || endDate == null)
-//            {
-//                if(AttributeTokenService.checkAccess(token,"root"))
-//                {
-//                    List<Shops> shops = shopRepository.findAll();
-//                    for(Shops shopEntity : shops)
-//                    {
-//                        ResShop resShop = new ResShop(shopEntity);
-//                        response.add(resShop);
-//                    }
-//                }
-//                else if(AttributeTokenService.checkAccess(token,"admin"))
-//                {
-//                    Users admin = userRepository.findByUsername(username);
-//                    List<Shops> shops = shopRepository.findByCreatedById(admin.getId());
-//                    for(Shops shopEntity : shops)
-//                    {
-//                        ResShop resShop = new ResShop(shopEntity);
-//                        response.add(resShop);
-//                    }
-//                }
-//                else if(AttributeTokenService.checkAccess(token,"manager"))
-//                {
-//                    Users manager = userRepository.findByUsername(username);
-//                    List<Shops> shops = shopRepository.findByOwnerId(manager.getId());
-//                    for(Shops shopEntity : shops)
-//                    {
-//                        ResShop resShop = new ResShop(shopEntity);
-//                        response.add(resShop);
-//                    }
-//                }
-//                else
-//                {
-//                    Date startDateFind = new SimpleDateFormat("dd/MM/yyyy").parse(startDate);
-//                    Date endDateFind = new SimpleDateFormat("dd/MM/yyyy").parse(endDate);
-//                    if(AttributeTokenService.checkAccess(token,"root"))
-//                    {
-//                        List<Shops> shops = shopRepository.findBetweenContractTerm(startDateFind, endDateFind);
-//                        for(Shops shopEntity : shops)
-//                        {
-//                            ResShop resShop = new ResShop(shopEntity);
-//                            response.add(resShop);
-//                        }
-//                    }
-//                    else if(AttributeTokenService.checkAccess(token,"admin"))
-//                    {
-//                        Users admin = userRepository.findByUsername(username);
-//                        List<Shops> shops = shopRepository
-//                                        .findByCreatedByIdAndBetweenContractTerm
-//                                        (admin.getId(),startDateFind,endDateFind);
-//                        for(Shops shopEntity : shops)
-//                        {
-//                            ResShop resShop = new ResShop(shopEntity);
-//                            response.add(resShop);
-//                        }
-//                    }
-//                    else if(AttributeTokenService.checkAccess(token,"manager"))
-//                    {
-//                        Users manager = userRepository.findByUsername(username);
-//                        List<Shops> shops = shopRepository.findByOwnerId(manager.getId());
-//                        for(Shops shopEntity : shops)
-//                        {
-//                            ResShop resShop = new ResShop(shopEntity);
-//                            response.add(resShop);
-//                        }
-//                    }
-//                }
-//            }
-        else
+            else
             {
+                int pageInt = Integer.parseInt(page);
+                int pagesizeInt = Integer.parseInt(pagesize);
+                if(startDate == null && endDate == null)
+                {
+                    if(AttributeTokenService.checkAccess(token,"root"))
+                    {
+                        shopsEntity = shopRepository.findAll(PageRequest.of(pageInt,pagesizeInt));
+                    }
+                    else if(AttributeTokenService.checkAccess(token,"admin"))
+                    {
+                        Users admin = userRepository.findByUsername(username);
+                        shopsEntity = shopRepository.findByCreatedBy(admin.getId(), PageRequest.of(pageInt, pagesizeInt));
+                    }
+                    else if(AttributeTokenService.checkAccess(token,"manager"))
+                    {
+                        Users manager = userRepository.findByUsername(username);
+                        shopsEntity = shopRepository.findByOwner(manager, PageRequest.of(pageInt, pagesizeInt));
+                    }
+                    else {
 
+                    }
+                }
+                else if(startDate != null && endDate ==null)
+                {
+                    Date startDateFind = new SimpleDateFormat("dd/MM/yyyy").parse(startDate);
+                    if(AttributeTokenService.checkAccess(token,"root"))
+                    {
+                        shopsEntity = shopRepository.findByContractTermAfter(startDateFind, PageRequest.of(pageInt, pagesizeInt));
+                    }
+                    else if(AttributeTokenService.checkAccess(token,"admin"))
+                    {
+                        Users admin = userRepository.findByUsername(username);
+                        shopsEntity = shopRepository.findByCreatedByAndContractTermAfter
+                                        (admin.getId(),startDateFind,  PageRequest.of(pageInt, pagesizeInt));
+                    }
+                    else if(AttributeTokenService.checkAccess(token,"manager"))
+                    {
+                        Users manager = userRepository.findByUsername(username);
+                        shopsEntity = shopRepository.findByOwnerAndContractTermAfter
+                                        (manager,startDateFind, PageRequest.of(pageInt, pagesizeInt) );
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else if(startDate == null && endDate != null)
+                {
+                    Date endDateFind = new SimpleDateFormat("dd/MM/yyyy").parse(endDate);
+                    if(AttributeTokenService.checkAccess(token,"root"))
+                    {
+                        shopsEntity = shopRepository.findByContractTermBefore(endDateFind, PageRequest.of(pageInt, pagesizeInt));
+                    }
+                    else if(AttributeTokenService.checkAccess(token,"admin"))
+                    {
+                        Users admin = userRepository.findByUsername(username);
+                        shopsEntity = shopRepository.findByCreatedByAndContractTermBefore
+                                        (admin.getId(),endDateFind, PageRequest.of(pageInt, pagesizeInt) );
+                    }
+                    else if(AttributeTokenService.checkAccess(token,"manager"))
+                    {
+                        Users manager = userRepository.findByUsername(username);
+                        shopsEntity = shopRepository.findByOwnerAndContractTermBefore
+                                        (manager,endDateFind, PageRequest.of(pageInt, pagesizeInt) );
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else
+                {
+                    Date startDateFind = new SimpleDateFormat("dd/MM/yyyy").parse(startDate);
+                    Date endDateFind = new SimpleDateFormat("dd/MM/yyyy").parse(endDate);
+                    if(AttributeTokenService.checkAccess(token,"root"))
+                    {
+                        shopsEntity = shopRepository.findByContractTermBetween(startDateFind, endDateFind, PageRequest.of(pageInt, pagesizeInt) );
+                    }
+                    else if(AttributeTokenService.checkAccess(token,"admin"))
+                    {
+                        Users admin = userRepository.findByUsername(username);
+                        shopsEntity = shopRepository
+                                        .findByCreatedByAndContractTermBetween
+                                        (admin.getId(),startDateFind,endDateFind, PageRequest.of(pageInt, pagesizeInt) );
+                    }
+                    else if(AttributeTokenService.checkAccess(token,"manager"))
+                    {
+                        Users manager = userRepository.findByUsername(username);
+                        shopsEntity = shopRepository.findByOwnerAndContractTermBetween(
+                                        manager,
+                                        startDateFind,
+                                        endDateFind,
+                                        PageRequest.of(pageInt, pagesizeInt)
+                                );
+                    }
+                }
+                if(shopsEntity != null)
+                {
+                    for(Shops shop : shopsEntity)
+                    {
+                        ResShop resShop = new ResShop(shop);
+                        response.add(resShop);
+                    }
+                }
+                else return null;
             }
             return response;
         } catch (Exception e)
@@ -201,4 +243,7 @@ public class ShopService {
             return null;
         }
     }
+
+
+
 }
