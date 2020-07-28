df5f101b3228d26ccf63edc4360054f127d4e365
thanhnh99 <thanhnh99.amc@gmail.com>
2020-07-28 11:04:09 +0700

    fix

--- src/main/java/com/multiple_language_menu/controllers/CategoryController.java
+++ src/main/java/com/multiple_language_menu/controllers/CategoryController.java
@@ -1,35 +1,38 @@
 package com.multiple_language_menu.controllers;
 
-import com.multiple_language_menu.services.AppAuthorizerImpl;
-import com.multiple_language_menu.services.AttributeTokenService;
-import com.multiple_language_menu.services.TokenAuthenticationService;
+import com.multiple_language_menu.services.authorize.AttributeTokenService;
+import com.multiple_language_menu.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.security.RolesAllowed;
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
+
     @Autowired
-    TokenAuthenticationService authenticationService;
+    CategoryService categoryService;
 
     @GetMapping()
-//    @RolesAllowed({ "ROOT" })
-    @PreAuthorize("@appAuthorizer.authorize(authentication, 'ROOT.ADMIN',this)")
-    public String getAll(HttpServletRequest request)
+//    @RolesAllowed({ "ROLE_ROOT" })
+    @PreAuthorize("@appAuthorizer.authorize(authentication, {'root', 'admin'})")
+    public String getCategories(HttpServletRequest request,
+                                @RequestParam(required = true) String page,
+                                @RequestParam(required = true) String pagesize,
+                                @RequestParam(required = false) String shopId)
     {
         String token = request.getHeader("Authorization");
-        AttributeTokenService.getRolesFromToken(token);
+        if(AttributeTokenService.checkAccess(token,"ROOT"))
+        {
+            //Service getCategoryRoot
+        }else if (AttributeTokenService.checkAccess(token,"ADMIN"))
+        {
+            //Service getCategoryAdmin
+        }
 
         return  "ALL CATEGORY";
-//        authenticationService.getRolesFromToken(request);
-//        return authenticationService.getRolesFromToken(request);
     }
 }
