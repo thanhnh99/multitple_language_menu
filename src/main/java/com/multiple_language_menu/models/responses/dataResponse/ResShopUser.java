d6be628227c0091cb1f3fd956d0af9845ec5be88
thanhnh99 <thanhnh99.amc@gmail.com>
2020-07-28 17:30:07 +0700

    refator ResShop for User

--- src/main/java/com/multiple_language_menu/models/responses/dataResponse/ResShopUser.java
+++ src/main/java/com/multiple_language_menu/models/responses/dataResponse/ResShopUser.java
@@ -1,4 +1,20 @@
 package com.multiple_language_menu.models.responses.dataResponse;
 
+import com.multiple_language_menu.models.entities.Shops;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResShopUser {
+    String shopName;
+    String shopId;
+
+    public ResShopUser(Shops shops)
+    {
+        this.shopName = shops.getName();
+        this.shopId = shops.getId();
+    }
 }
