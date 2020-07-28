8171bb1f4cc60211cb62b93f864ee3a349b546b5
thanhnh99 <thanhnh99.amc@gmail.com>
2020-07-28 11:03:00 +0700

    init Item Controller

--- src/main/java/com/multiple_language_menu/controllers/ItemController.java
+++ src/main/java/com/multiple_language_menu/controllers/ItemController.java
@@ -1,7 +1,10 @@
 package com.multiple_language_menu.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/item")
 public class ItemController {
 }
