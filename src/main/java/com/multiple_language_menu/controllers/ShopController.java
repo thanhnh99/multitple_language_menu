839f7f2b5ea532ea2f205c7095fcfe02c3ca673c
thanhnh99 <thanhnh99.amc@gmail.com>
2020-07-29 15:53:14 +0700

    edit Shop info

--- src/main/java/com/multiple_language_menu/controllers/ShopController.java
+++ src/main/java/com/multiple_language_menu/controllers/ShopController.java
@@ -1,14 +1,17 @@
 package com.multiple_language_menu.controllers;
 
 import com.multiple_language_menu.models.request.ReqCreateShop;
+import com.multiple_language_menu.models.request.ReqEditShop;
 import com.multiple_language_menu.models.responses.dataResponse.ResShop;
 import com.multiple_language_menu.models.responses.httpResponse.HttpResponse;
 import com.multiple_language_menu.services.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.ParseException;
 import java.util.List;
 
 @RestController
@@ -18,6 +21,7 @@ public class ShopController {
     ShopService shopService;
 
     @PostMapping()
+    @PreAuthorize("@appAuthorizer.authorize(authentication, {'root', 'admin'})")
     public ResponseEntity<HttpResponse> addShop(HttpServletRequest httpRequest, @RequestBody ReqCreateShop requestData)
     {
         HttpResponse<ResShop> response = new HttpResponse();
@@ -36,6 +40,7 @@ public class ShopController {
     }
 
     @GetMapping()
+    @PreAuthorize("@appAuthorizer.authorize(authentication, {'root', 'admin', 'manager'})")
     public ResponseEntity<HttpResponse> getShops(HttpServletRequest httpRequest,
                                                  @RequestParam(required = false) String page,
                                                  @RequestParam(required = false) String pagesize,
@@ -57,5 +62,41 @@ public class ShopController {
         return ResponseEntity.status(400).body(response);
     }
 
+    @PutMapping("{shopId}")
+    @PreAuthorize("@appAuthorizer.authorize(authentication, {'root', 'admin', 'manager'})")
+    public ResponseEntity<HttpResponse> editShop(HttpServletRequest httpRequest,
+                                                 @RequestBody ReqEditShop requestData,
+                                                 @PathVariable String shopId) throws ParseException
+    {
+        HttpResponse<List<ResShop>> response = new HttpResponse();
+        if(shopService.editShop(httpRequest, requestData,shopId))
+        {
+            response.setStatusCode("200");
+            response.setMessage("success");
+            response.setData(null);
+            return ResponseEntity.ok(response);
+        }
+        response.setStatusCode("400");
+        response.setMessage("bad request");
+        response.setData(null);
+        return ResponseEntity.status(400).body(response);
+    }
+
+    @DeleteMapping("{shopId}")
+    @PreAuthorize("@appAuthorizer.authorize(authentication, {'root', 'admin', 'manager'})")
+    public ResponseEntity<HttpResponse> deleteShop(HttpServletRequest httpRequest,
+                                                   @PathVariable String shopId)
+    {
+        HttpResponse<List<ResShop>> response = new HttpResponse();
+        if(shopService.deleteShop(httpRequest ,shopId))
+        {
+            response.setStatusCode("200");
+            response.setMessage("success");
+            return ResponseEntity.ok(response);
+        }
+        response.setStatusCode("400");
+        response.setMessage("bad request");
+        return ResponseEntity.status(400).body(response);
+    }
 
 }
