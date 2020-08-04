332bdf939e6c027cb94ae71f427f56bdf29266a9
thanhnh99 <thanhnh99.amc@gmail.com>
2020-08-04 16:00:19 +0700

    fix bug

--- src/main/java/com/multiple_language_menu/repositories/ILogRepository.java
+++ src/main/java/com/multiple_language_menu/repositories/ILogRepository.java
@@ -1,13 +1,13 @@
-package com.multiple_language_menu.repositories;
-
-import com.multiple_language_menu.models.entities.Logs;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.awt.print.Pageable;
-import java.util.List;
-
-@Repository
-public interface ILogRepository extends JpaRepository<Logs, String> {
-    List<Logs> findAllByCreatedAtDESC(Pageable pageable);
-}
+//package com.multiple_language_menu.repositories;
+//
+//import com.multiple_language_menu.models.entities.Logs;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.awt.print.Pageable;
+//import java.util.List;
+//
+//@Repository
+//public interface ILogRepository extends JpaRepository<Logs, String> {
+//    List<Logs> findAllByCreatedAtDESC(Pageable pageable);
+//}
