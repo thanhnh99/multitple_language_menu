9e69fc7f8d678a504f1d42d6f528951e7b676b8d
thanhnh99 <thanhnh99.amc@gmail.com>
2020-08-04 15:59:11 +0700

    add contructor from Categori entity

--- src/main/java/com/multiple_language_menu/models/responses/dataResponse/ResCategory.java
+++ src/main/java/com/multiple_language_menu/models/responses/dataResponse/ResCategory.java
@@ -1,10 +1,12 @@
 package com.multiple_language_menu.models.responses.dataResponse;
 
+import com.multiple_language_menu.models.entities.Categories;
 import com.multiple_language_menu.models.entities.CategoriesTranslates;
 
 import java.util.List;
 
 public class ResCategory {
+    private Integer rank;
     private String categoryId;
     private String name;
     private String description;
@@ -12,9 +14,18 @@ public class ResCategory {
 
     public ResCategory(CategoriesTranslates categoriesTranslates)
     {
+        this.rank = categoriesTranslates.getCategory().getRank();
         this.categoryId = categoriesTranslates.getCategory().getId();
         this.name = categoriesTranslates.getName();
         this.description = categoriesTranslates.getDescription();
+    }
+
+    public ResCategory(Categories category)
+    {
+        this.categoryId = category.getId();
+        this.name = category.getName();
+        this.description = category.getDescription();
+        this.rank = category.getRank();
     }
 }
 
