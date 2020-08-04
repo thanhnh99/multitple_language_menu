ce8330822d05449cd0e7d6590581336cc108a919
thanhnh99 <thanhnh99.amc@gmail.com>
2020-08-04 11:20:38 +0700

    category refactor

--- src/main/java/com/multiple_language_menu/controllers/CategoryController.java
+++ src/main/java/com/multiple_language_menu/controllers/CategoryController.java
@@ -1,8 +1,15 @@
 package com.multiple_language_menu.controllers;
 
-import com.multiple_language_menu.services.authorize.AttributeTokenService;
+import com.multiple_language_menu.models.request.ReqCreateCategory;
+import com.multiple_language_menu.models.request.ReqEditItem;
+import com.multiple_language_menu.models.responses.dataResponse.ResCategory;
+import com.multiple_language_menu.models.responses.dataResponse.ResItem;
+import com.multiple_language_menu.models.responses.httpResponse.HttpResponse;
 import com.multiple_language_menu.services.CategoryService;
+import com.multiple_language_menu.services.authorize.AttributeTokenService;
+import org.hibernate.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,7 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
-@RequestMapping("/categories")
+@RequestMapping("/category")
 public class CategoryController {
 
     @Autowired
@@ -35,4 +42,56 @@ public class CategoryController {
 
         return  "ALL CATEGORY";
     }
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<HttpResponse<List<Object>>> getChildFromCategory(
+            @PathVariable String categoryId,
+            @RequestParam("languageCode") String languageCode
+    )
+    {
+        HttpResponse<List<Object>> httpResponse = new HttpResponse<>();
+        List<Object> responseData = categoryService.getChildByCategoryId(categoryId,languageCode);
+        if(responseData != null)
+        {
+            httpResponse.setStatusCode("200");
+            httpResponse.setMessage("success");
+            httpResponse.setData(responseData);
+            return ResponseEntity.status(200).body(httpResponse);
+
+        }
+        httpResponse.setStatusCode("400");
+        httpResponse.setMessage("bad request");
+        httpResponse.setData(null);
+        return ResponseEntity.status(400).body(httpResponse);
+    }
+
+    @PutMapping
+//    @PreAuthorize("@appAuthorizer.authorize(authentication, {'root', 'admin', 'manager'})")
+    public ResponseEntity<String> editCategory(HttpServletRequest httpRequest,
+                                               @RequestBody ReqEditItem requestData)
+    {
+        //check categoryList
+        String response = requestData.getClass().getName();
+        return ResponseEntity.ok(response);
+    }
+
+
+    @PostMapping
+//    @PreAuthorize("@appAuthorizer.authorize(authentication, {'root', 'admin', 'manager'})")
+    public ResponseEntity<HttpResponse> createCategory(HttpServletRequest httpRequest,
+//                                                     @PathVariable String categoryId,
+                                                       @RequestBody ReqCreateCategory requestData)
+    {
+        HttpResponse response = new HttpResponse();
+        if (categoryService.createCategory(httpRequest, requestData))
+        {
+            response.setStatusCode("200");
+            response.setMessage("success");
+            return ResponseEntity.status(200).body(response);
+        }
+        response.setStatusCode("400");
+        response.setMessage("bad request");
+        return ResponseEntity.status(200).body(response);
+    }
+
 }
