1db3b616a754c54906880a07e2354b4ed880606b
thanhnh99 <thanhnh99.amc@gmail.com>
2020-08-05 15:29:21 +0700

    add constructor

--- src/main/java/com/multiple_language_menu/models/request/ReqSendMail.java
+++ src/main/java/com/multiple_language_menu/models/request/ReqSendMail.java
@@ -1,7 +1,11 @@
 package com.multiple_language_menu.models.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class ReqSendMail {
     private String sendTo;
