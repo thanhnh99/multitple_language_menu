d247fb3b9e45b8bb7de259082d4bdd1abb1d146b
thanhnh99 <thanhnh99.amc@gmail.com>
2020-08-06 18:08:23 +0700

    add request mapping

--- src/main/java/com/multiple_language_menu/controllers/AuthController.java
+++ src/main/java/com/multiple_language_menu/controllers/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -17,6 +18,7 @@ import java.util.Collection;
 import java.util.Collections;
 
 @RestController
+@RequestMapping("")
 public class AuthController {
     @Autowired
     AuthService authService;
