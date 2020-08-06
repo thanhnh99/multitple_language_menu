128bd1ca32619efd059e252a32672b377c361f1a
thanhnh99 <thanhnh99.amc@gmail.com>
2020-08-06 18:09:26 +0700

    get log

--- src/main/java/com/multiple_language_menu/controllers/LogController.java
+++ src/main/java/com/multiple_language_menu/controllers/LogController.java
@@ -1,4 +1,33 @@
 package com.multiple_language_menu.controllers;
 
+import com.multiple_language_menu.models.responses.dataResponse.ResLog;
+import com.multiple_language_menu.models.responses.httpResponse.HttpResponse;
+import com.multiple_language_menu.services.LogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+@RequestMapping("log")
 public class LogController {
+    @Autowired
+    LogService logService;
+
+    @GetMapping
+    @PreAuthorize("@appAuthorizer.authorize(authentication, {'root', 'admin', 'manager'})")
+    public ResponseEntity<HttpResponse<List<ResLog>>> getLogs(HttpServletRequest request)
+    {
+        HttpResponse<List<ResLog>> httpResponse = new HttpResponse<>();
+        List<ResLog> responseData = logService.getLogs(request);
+        httpResponse.setStatusCode("200");
+        httpResponse.setMessage("success");
+        httpResponse.setData(responseData);
+        return ResponseEntity.status(200).body(httpResponse);
+    }
 }
